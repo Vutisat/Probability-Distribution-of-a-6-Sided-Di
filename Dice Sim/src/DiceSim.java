bc562001c24d33b8f6f83d83e9e64abf6d19af27
Vutisat <vutisat@miamioh.edu>
2014-05-23 05:03:23 -0400

    Initial commit

---
+++ Dice Sim/src/DiceSim.java
@@ -0,0 +1,17 @@
+// Author: Vutisat
+
+public class DiceSim {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static void rollingDice(int n){
+		
+	}
+
+}
